9bb05c2867514055b729317d1b85b7a500c001d2
Jamie Corrigan <38882200+jamrig@users.noreply.github.com>
2022-11-21 21:29:48 +1000

    cleanup memory

--- emulator/memory.go
+++ emulator/memory.go
@@ -3,13 +3,13 @@ package emulator
 // Memory represents the RAM.
 type Memory struct {
 	// Size is the size of the memory in bytes.
-	Size int
+	Size uint16
 	// Data is the raw byte data.
 	Data []byte
 }
 
 // NewMemory returns a new Memory.
-func NewMemory(size int) *Memory {
+func NewMemory(size uint16) *Memory {
 	m := &Memory{
 		Size: size,
 		Data: make([]byte, size),
@@ -19,18 +19,28 @@ func NewMemory(size int) *Memory {
 }
 
 // Write bytes starting at a specific address.
-func (m *Memory) Write(addr int, val []byte) {
+func (m *Memory) Write(addr uint16, val []byte) {
 	if addr < 0 || addr >= m.Size {
 		// TODO: log
 		return
 	}
 
-	if (len(val) + addr) > m.Size {
+	if (uint16(len(val)) + addr) > m.Size {
 		// TODO: log
 		return
 	}
 
 	for i, v := range val {
-		m.Data[addr+i] = v
+		m.Data[addr+uint16(i)] = v
 	}
+}
+
+// Read bytes at a specific address.
+func (m *Memory) Read(addr uint16) byte {
+	if addr < 0 || addr >= m.Size {
+		// TODO: log
+		return 0
+	}
+
+	return m.Data[addr]
 }
